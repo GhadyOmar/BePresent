969942bb7f7bd28ae4247cbe4de7685623e061f2
GhadyOmar <90251825+GhadyOmar@users.noreply.github.com>
2026-05-07 09:58:24 +0300

    Initial Commit

---
+++ BePresent/BePresentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BePresentApp.swift
+//  BePresent
+//
+//  Created by Ghady Al Omar on 20/11/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct BePresentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BePresentTests/BePresentTests.swift
@@ -0,0 +1,17 @@
+//
+//  BePresentTests.swift
+//  BePresentTests
+//
+//  Created by Ghady Al Omar on 20/11/1447 AH.
+//
+
+import Testing
+@testable import BePresent
+
+struct BePresentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
